0111fb9714fd56cce5f35fd44ba2d19015e5db42
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-11-02 15:35:24 +0530

    Working On main Layout

--- src/Layouts/EqualLayout.jsx
+++ src/Layouts/EqualLayout.jsx
@@ -73,7 +73,7 @@ function EqualLayout({ children }) {
       <div className="flex justify-center items-center w-full border-b-[1.852px] mt-[2px]">
         <ul className="flex justify-center items-center h-[90px] font-roboto">
 
-            <li className="hover:bg-[#919191] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#919191] after:rounded-bl-lg hover:text-white transition-colors duration-200">
+            <li className="hover:bg-[#919191] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#919191] after:rounded-bl-lg hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={Pikachu} alt="pikachuNav" />
@@ -82,7 +82,7 @@ function EqualLayout({ children }) {
                 <span className="my-[5px] text-sm w-full text-center ">Home</span>
             </li>
 
-            <li className="hover:bg-[#ff0000ed] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#ff0000ed] hover:text-white">
+            <li className="hover:bg-[#ff0000ed] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#ff0000ed] hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={Pokeball} alt="pikachuNav" />
@@ -91,7 +91,7 @@ function EqualLayout({ children }) {
                 <span className="my-[5px] text-sm w-full text-center">Pokédex</span>
             </li>
 
-            <li className="hover:bg-[#f98955] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#f98955] hover:text-white">
+            <li className="hover:bg-[#f98955] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#f98955] hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={Game} alt="pikachuNav" />
@@ -100,7 +100,7 @@ function EqualLayout({ children }) {
                 <span className="my-[5px] text-sm leading-[14.5px] w-full text-center">Video Games & Apps</span>
             </li>
 
-            <li className="hover:bg-[#a915ff] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#a915ff] hover:text-white">
+            <li className="hover:bg-[#a915ff] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#a915ff] hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={"/icons/pokecard.svg"} alt="pikachuNav" />
@@ -109,7 +109,7 @@ function EqualLayout({ children }) {
                 <span className="my-[5px] text-sm w-full text-center">Trading Card Game</span>
             </li>
 
-            <li className="hover:bg-[#4dad5b] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#4dad5b] hover:text-white">
+            <li className="hover:bg-[#4dad5b] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#4dad5b] hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={"/icons/pokemonTv.svg"} alt="pikachuNav" />
@@ -118,7 +118,7 @@ function EqualLayout({ children }) {
                 <span className="my-[5px] text-sm w-full text-center">Pokémon TV</span>
             </li>
 
-            <li className="hover:bg-[#30a7d7] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#30a7d7] hover:text-white">
+            <li className="hover:bg-[#30a7d7] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#30a7d7] hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={"/icons/trophy.svg"} alt="pikachuNav" />
@@ -127,7 +127,7 @@ function EqualLayout({ children }) {
                 <span className="my-[5px] text-sm leading-[14.5px] w-full text-center">Play! Pokémon Events</span>
             </li>
 
-            <li className="hover:bg-[#1b53ba] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#1b53ba] after:rounded-br-lg hover:text-white">
+            <li className="hover:bg-[#1b53ba] flex flex-col cursor-pointer w-[127.853px] h-full relative after:h-[6px] after:content-[' '] after:absolute after:w-full after:bottom-[-6px] after:z-[1] after:bg-[#1b53ba] after:rounded-br-lg hover:text-white transition-colors duration-100 ease-in">
 
                 <span className="h-[30.9977px] flex justify-center items-center w-full mt-[15px]">
                   <img className="h-full aspect-auto" src={"/icons/news.svg"} alt="pikachuNav" />
