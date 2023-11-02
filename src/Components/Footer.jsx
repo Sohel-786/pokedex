c3f762b63935ab7e341fa656489cbdb075b57aac
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-11-02 21:16:38 +0530

    Working On main Layout

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,26 +1,25 @@
-import { FaSquarePinterest } from "react-icons/fa6";
-import { AiFillTwitterSquare } from "react-icons/ai";
-import { ImFacebook2 } from "react-icons/im";
-import { FaYoutubeSquare } from "react-icons/fa";
-
 function Footer() {
   return (
     <div className="mt-5">
       <div></div>
 
-      <div className="bg-[#1f1f1f] text-white" style={{
-        userSelect : 'none'
-      }}>
-        <div className="flex">
-          <div>
-            <h1>
+      <div
+        className="bg-[#1f1f1f] text-white w-full flex justify-center"
+        style={{
+          userSelect: "none",
+        }}
+      >
+        <div className="flex max-w-[1024px] gap-5 justify-center">
+
+          <div className="w-[27.46%] mt-7 float-left border-r-[2px] border-[#000]">
+            <h1 className="mx-[8.800px] mb-[17.600px]">
               <img src="/assets/pokecompany.png" alt="pokecompany" />
             </h1>
 
-            <ul>
+            <ul className="flex flex-col">
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   News
@@ -28,7 +27,7 @@ function Footer() {
               </li>
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   Parents Guide
@@ -36,7 +35,7 @@ function Footer() {
               </li>
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   Customer Service
@@ -44,7 +43,7 @@ function Footer() {
               </li>
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   About Our Company
@@ -52,7 +51,7 @@ function Footer() {
               </li>
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   Careers
@@ -60,7 +59,7 @@ function Footer() {
               </li>
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   Select a Country/Region
@@ -68,7 +67,7 @@ function Footer() {
               </li>
               <li>
                 <a
-                  className="ml-[8] mb-[8] pl-8 font-openSans text-[15px] hover:underline"
+                  className="ml-[8px] mb-[8px] pl-[8px] font-openSans text-[15px] hover:underline"
                   href="#"
                 >
                   Press Site
@@ -77,25 +76,14 @@ function Footer() {
             </ul>
           </div>
 
-          <div className="flex justify-center">
-            <ul className="flex">
+          <div className="flex justify-center w-[27.46%] mt-7 float-left border-r-[2px] border-[#000]">
+            <ul className="flex gap-1">
               <li className="">
                 <div
                   className="h-[33.9988px] w-[35px] mr-[6.4px] mb-[8px] cursor-pointer hover:opacity-80"
                   style={{
                     backgroundImage: 'url("/footerIcon/social-icons.png")',
                     backgroundAttachment: "scroll",
-                  }}
-                ></div>
-              </li>
-              
-              <li className="">
-                <div
-                  className="h-[33.9988px] w-[35px] mr-[6.4px] mb-[8px] cursor-pointer hover:opacity-80"
-                  style={{
-                    backgroundImage: 'url("/footerIcon/social-icons.png")',
-                    backgroundAttachment: "scroll",
-                    backgroundPositionX : '-175px'
                   }}
                 ></div>
               </li>
@@ -106,7 +94,7 @@ function Footer() {
                   style={{
                     backgroundImage: 'url("/footerIcon/social-icons.png")',
                     backgroundAttachment: "scroll",
-                    backgroundPositionX : '-70px'
+                    backgroundPositionX: "-175px",
                   }}
                 ></div>
               </li>
@@ -117,7 +105,7 @@ function Footer() {
                   style={{
                     backgroundImage: 'url("/footerIcon/social-icons.png")',
                     backgroundAttachment: "scroll",
-                    backgroundPositionX : '-210px'
+                    backgroundPositionX: "-70px",
                   }}
                 ></div>
               </li>
@@ -128,14 +116,62 @@ function Footer() {
                   style={{
                     backgroundImage: 'url("/footerIcon/social-icons.png")',
                     backgroundAttachment: "scroll",
-                    backgroundPositionX : '-245px',
+                    backgroundPositionX: "-210px",
+                  }}
+                ></div>
+              </li>
+
+              <li className="">
+                <div
+                  className="h-[33.9988px] w-[35px] mr-[6.4px] mb-[8px] cursor-pointer hover:opacity-80"
+                  style={{
+                    backgroundImage: 'url("/footerIcon/social-icons.png")',
+                    backgroundAttachment: "scroll",
+                    backgroundPositionX: "-245px",
                   }}
                 ></div>
               </li>
             </ul>
           </div>
 
-          <div></div>
+          <div className="w-[27.46%] mt-7 float-left px-[20.388px]">
+            <div>
+              <div>
+                <img src="/assets/caru.png" alt="caru" />
+              </div>
+              <ul className="font-roboto text-[11.008px]">
+                <li className="py-[5.504px]">
+                  <a className="hover:underline" href="#">
+                    Terms of Use
+                  </a>
+                </li>
+                <li className="py-[5.504px]">
+                  <a className="hover:underline" href="#">
+                    Privacy Notice
+                  </a>
+                </li>
+                <li className="py-[5.504px]">
+                  <a className="hover:underline" href="#">
+                    Cookie Page
+                  </a>
+                </li>
+                <li className="py-[5.504px]">
+                  <a className="hover:underline" href="#">
+                    Legal Info
+                  </a>
+                </li>
+                <li className="py-[5.504px]">
+                  <a className="hover:underline" href="#">
+                    Security
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <p className="p-[5] font-roboto text-[11.008px] leading-[11.5px]">
+              ©2023 Pokémon. ©1995 - 2023 Nintendo/Creatures Inc./GAME FREAK
+              inc. TM, ®Nintendo.
+            </p>
+          </div>
         </div>
       </div>
     </div>
