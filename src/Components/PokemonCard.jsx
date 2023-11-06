1bd64a6de074b8956b0ce341a6615fde4ace145a
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-11-06 16:55:55 +0530

    Working on Pokedex

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,13 +1,36 @@
-function PokemonCard(){
-    return (
-        <li className="mx-[6.83449px] mb-[50px] text-left w-[205.174px]">
-            <div className="w-full bg-[#F2F2F2] rounded-[5px] ">
-                <img className="w-full aspect-auto" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png" alt="Bulbasaur" />
-            </div>
-            <p className="font-sans text-[#919191] text-[12.8px] leading-4 pt-[2px] my-[6.400px] font-bold"><span>#</span>0001</p>
-            <h1 className="mt-[11.600px] mb-[5px] text-[#313131] text-[23.2px] leading-[29px] font-openSans bg-white font-bold">Bulbasaur</h1>
-        </li>   
-    )
+import PokemonType from "./PokemonType";
+import { nanoid } from "nanoid";
+
+function PokemonCard() {
+  return (
+    <li className="mx-[6.83449px] mb-[50px] text-left w-[205.174px]">
+      <div className="w-full bg-[#F2F2F2] rounded-[5px] ">
+        <img
+          className="w-full aspect-auto"
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png"
+          alt="Bulbasaur"
+        />
+      </div>
+      <p className="font-sans text-[#919191] text-[12.8px] leading-4 pt-[2px] my-[6.400px] font-bold">
+        <span>#</span>0001
+      </p>
+      <h1 className="mt-[11.600px] mb-[5px] text-[#313131] text-[23.2px] leading-[29px] font-openSans bg-white font-bold">
+        Bulbasaur
+      </h1>
+      <ul className="flex flex-wrap">
+        {["grass", "poison", "bug", "fire", "fighting", "water", "ice", "dark", "rock", "dragon", "flying", "ground", "electric", "psychic", "fairy", "normal", "ghost"].map((el) => {
+          return (
+            <PokemonType
+              key={nanoid(4)}
+              type={el}
+              leading={"18px"}
+              fontSize={"11px"}
+            />
+          );
+        })}
+      </ul>
+    </li>
+  );
 }
 
 export default PokemonCard;
--- src/Components/PokemonType.jsx
+++ src/Components/PokemonType.jsx
@@ -1,27 +1,94 @@
 function PokemonType({ type, leading, fontSize }) {
   if (type === "grass") {
-    return <li className={`mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-tr from-[#9bcc50] to-green-500 rounded-[3px]`}>
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-tr from-[#9bcc50] to-green-500 rounded-[3px]`}>
         Grass
     </li>;
   }
   else if (type === "poison") {
-    return <li className={`mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#b97fc9] to-purple-500 rounded-[3px]`}>
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#b97fc9] to-purple-500 rounded-[3px]`}>
         Poison
     </li>;
   }
   else if (type === "fire") {
-    return <li className={`mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#fd7d24] to-orange-600 rounded-[3px]`}>
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#fd7d24] to-orange-600 rounded-[3px]`}>
         Fire
     </li>;
   }
   else if (type === "flying") {
-    return <li className={`mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-b from-[#3dc7ef] from-50% to-[#bdb9b8] to-50% rounded-[3px]`}>
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-b from-[#3dc7ef] from-50% to-[#bdb9b8] to-50% rounded-[3px]`}>
         Flying
     </li>;
   }
   else if (type === "water") {
-    return <li className={`mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-b from-[#3dc7ef] from-50% to-[#bdb9b8] to-50% rounded-[3px]`}>
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#4592c4] to-sky-600 rounded-[3px]`}>
         Water
     </li>;
   }
+  else if (type === "bug") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#729f3f] to-green-600 rounded-[3px]`}>
+        Bug
+    </li>;
+  }
+  else if (type === "normal") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-tr from-[#a4acaf] to-gray-500 rounded-[3px]`}>
+        Normal
+    </li>;
+  }
+  else if (type === "electric") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-tr from-[#eed535] to-yellow-500 rounded-[3px]`}>
+        Electric
+    </li>;
+  }
+  else if (type === "ground") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-b from-[#f7de3f] from-50% to-[#ab9842] to-50% rounded-[3px]`}>
+        Ground
+    </li>;
+  }
+  else if (type === "fairy") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-tr from-[#fdb9e9] to-pink-400 rounded-[3px]`}>
+        Fairy
+    </li>;
+  }
+  else if (type === "fighting") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#d56723] to-orange-400 rounded-[3px]`}>
+        Fighting
+    </li>;
+  }
+  else if (type === "psychic") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-[#f366b9] rounded-[3px]`}>
+        Psychic
+    </li>;
+  }
+  else if (type === "rock") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-stone-500 to-stone-700 rounded-[3px]`}>
+        Rock
+    </li>;
+  }
+  else if (type === "steel") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-[#9eb7b8] rounded-[3px]`}>
+        Steel
+    </li>;
+  }
+  else if (type === "ice") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-black bg-gradient-to-tr from-[#51c4e7] to-blue-500 rounded-[3px]`}>
+        Ice
+    </li>;
+  }
+  else if (type === "ghost") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-tr from-[#7b62a3] to-purple-600 rounded-[3px]`}>
+        Ghost
+    </li>;
+  }
+  else if (type === "dragon") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-b from-[#53a4cf] from-50% to-[#f16e57] to-50% rounded-[3px]`}>
+        Dragon
+    </li>;
+  }
+  else if (type === "dark") {
+    return <li className={`list-none mr-[2.969px] w-[38.4375%] font-sans leading-[${leading}] text-[${fontSize}] text-center text-white bg-gradient-to-b from-[#707070] from-50% to-[#242424] to-50% rounded-[3px]`}>
+        Dark
+    </li>;
+  }
 }
+
+export default PokemonType;
