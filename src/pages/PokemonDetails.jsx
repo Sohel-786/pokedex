6198f7b756218e0c7dae92fb3fc3f7b1db4cc0cf
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-11-09 21:33:35 +0530

    Working on Pokedex

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -35,11 +35,38 @@ function PokemonDetails() {
             </div>
           </div>
 
-          <div className="bg-white h-[51px] absolute bottom-0 w-[58.0325%] ml-[20.2%] before:bg-pokedetails before:bg-no-repeat before:absolute before:h-[52px] before:w-[75px] before:content-[' '] before:bottom-0 before:left-[-75px] after:bg-pokedetails after:bg-no-repeat after:absolute after:h-[52px] after:w-[75px] after:content-[' '] after:bottom-0 after:right-[-75px] after:bg-right-top">
-            <div className="w-full mt-[18px] text-black">
-
+          <div className="bg-white h-[51px] absolute bottom-0 w-[58.0325%] ml-[20.2%] before:bg-pokedetails before:bg-no-repeat before:absolute before:h-[51.6px] before:w-[75px] before:content-[' '] before:bottom-0 before:left-[-75px] after:bg-pokedetails after:bg-no-repeat after:absolute after:h-[51.6px] after:w-[75px] after:content-[' '] after:bottom-0 after:right-[-75px] after:bg-right-top">
+            <div className="w-full mt-[18px] text-black text-center text-[225%] leading-[36px]" style={{
+                fontFamily : 'sans-serif'
+            }}>
+                    Bulbasaur 
+                    <span className="text-[#616161] ml-[20.500px] tracking-wide leading-[36px]">#0001</span>
             </div>
           </div>
+        </div>
+
+        <div className="w-full mx-auto pt-4 pb-[50px]">
+
+        </div>
+
+        <div className="w-full py-4 flex">
+            <div className="w-[49%] flex flex-col">
+                <div className="w-full rounded-[5px] bg-[#F2F2F2] ">
+                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="demo" className="w-full pb-[30px]" />
+                </div>
+
+                <div className="my-4 bg-[#a4a4a4] w-full rounded-[10px] ">
+                    <h1 className="m-6 mt-3 text-[#313131] text-base leading-5" style={{
+                      fontFamily : "sans-serif"
+                    }}>
+                      Stats
+                    </h1>
+
+                    <ul className="clear-both relative w-full list-none">
+                        <li className="mb-[20px] ml-[31.157px] mr-[6.713px]"></li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <SearchPokemon />
       </section>
