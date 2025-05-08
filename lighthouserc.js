998cc94cb8ff98fb70ab2b1be985daf60af5ca10
sohelshaikh-786 <sohel.s@empiricinfotech.com>
2025-05-08 12:30:25 +0530

    Azure ci cd pipeline change

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,13 +1,17 @@
 export default {
-    ci: {
-      collect: {
-        staticDistDir: './dist',
+    "ci": {
+      "collect": {
+        "staticDistDir": "./dist"
       },
-      assert: {
-        assertions: {
-          'categories:performance': ['error', { minScore: 0.7 }],
-        },
-      },
-    },
-  };
+      "assert": {
+        "preset": "lighthouse:recommended",
+        "assertions": {
+          "categories:performance": ["error", { "minScore": 0.7 }],
+          "categories:accessibility": ["warn", { "minScore": 0.7 }],
+          "categories:best-practices": ["warn", { "minScore": 0.7 }],
+          "categories:seo": ["warn", { "minScore": 0.7 }]
+        }
+      }
+    }
+  }
   
