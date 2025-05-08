356ca58ae102c75dd3e3e6d24854977271b8fb1c
sohelshaikh-786 <sohel.s@empiricinfotech.com>
2025-05-08 15:52:13 +0530

    Azure devops pipeline update

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,14 +5,6 @@ export default {
             numberOfRuns: 3,
             url: ['http://localhost:8080'], // fallback default if not running autorun
           },
-      "assert": {
-        "assertions": {
-          'categories:performance': ['error', { minScore: 0.6 }],
-          "categories:accessibility": ["warn", { "minScore": 0.7 }],
-          "categories:best-practices": ["warn", { "minScore": 0.7 }],
-          "categories:seo": ["warn", { "minScore": 0.7 }]
-        }
-      },
       "upload": {
         "target": "temporary-public-storage"
       }
